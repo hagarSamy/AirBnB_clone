9ae0eca860b859af295f44fd793ecc0de73b5c4d
hagarSamy <hs1800@fayoum.edu.eg>
2024-02-13 13:39:40 +0200

    A unittest for save

--- models/base_model.py
+++ models/base_model.py
@@ -17,11 +17,6 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-    def __str__(self):
-        '''Beautifying the printed output'''
-
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-
     def save(self):
         '''Upadtes updated_at to current date time'''
 
@@ -30,8 +25,12 @@ class BaseModel:
     def to_dict(self):
         '''Returns a dictionary'''
 
-        mydict = self.__dict__.copy()
-        mydict["created_at"] = self.created_at.isoformat()
-        mydict["updated_at"] = self.updated_at.isoformat()
-        mydict["__class__"] = self.__class__.__name__
-        return (mydict)
+        self.__dict__["created_at"] = self.created_at.isoformat()
+        self.__dict__["updated_at"] = self.updated_at.isoformat()
+        self.__dict__["__class__"] = self.__class__.__name__
+        return (self.__dict__)
+
+    def __str__(self):
+        '''Beautifying the printed output'''
+
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
