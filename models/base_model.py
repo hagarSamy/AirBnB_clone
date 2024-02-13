2bac81c7257605c224dba74cdba479d94ae4e09f
hagarSamy <hs1800@fayoum.edu.eg>
2024-02-13 10:11:23 +0200

    a new update with unittests

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,7 @@ from datetime import datetime
 class BaseModel:
     '''The baseModel class'''
 
-    def __init__(self, id, created_at, updated_at):
+    def __init__(self):
         '''Initiation of the base class
         '''
 
@@ -30,13 +30,10 @@ class BaseModel:
     def to_dict(self):
         '''Returns a dictionary'''
 
-        mydict = {}
-        instances = ("id", "created_at", "updated_at")
-        for key, value in self.__dict__.items():
-            if key in instances and value is not None:
-                key_formatted = key
-                if key == "created_at" or key == "updated_at":
-                    key_formatted = value.isoformat()
-                mydict[key_formatted] = repr(value)
-        mydict['__class__'] = repr(self.__class__.__name__)
+        mydict = {
+        "id": self.id,
+        "created_at": self.created_at.isoformat(),
+        "updated_at": self.updated_at.isoformat(),
+        "__class__": self.__class__.__name__
+        }
         return mydict
