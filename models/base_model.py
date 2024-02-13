9d0cabfeb140f58e9373709c51f932f175e92b02
hagarSamy <hs1800@fayoum.edu.eg>
2024-02-13 19:51:46 +0200

    handling kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -13,13 +13,13 @@ class BaseModel:
         '''Initiation of the base class
         '''
 
-        if kwargs is not None:
+        if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            self.id = kwargs['id']
-            self.created_at = kwargs['created_at']
+            self.id = kwargs["id"]
+            self.created_at = kwargs["created_at"]
 
     def __str__(self):
         '''Beautifying the printed output'''
