85c00c8feb65fd8241148bb90c2405d804f97ada
aya-cs22 <aya786930@gmail.com>
2024-02-13 15:31:49 +0200

    commit to airbnb where base is partially handled

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,5 +1,5 @@
 import unittest
-from datetime import datetime
+from datetime import timedelta
 from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
@@ -8,22 +8,17 @@ class TestBaseModel(unittest.TestCase):
     def setUp(self):
         '''A method for instanciation'''
         self.myinstance = BaseModel()
-
-    def test_to_dict(self):
-        '''Testing the to_dict method of the BaseModel class.'''
-        my_model_dict = self.myinstance.to_dict()
-        self.assertIsInstance(my_model_dict, dict)
-        self.assertIn('id', my_model_dict)
-        self.assertIn('created_at', my_model_dict)
-        self.assertIn('updated_at', my_model_dict)
-        self.assertIn('_class_', my_model_dict)
-        self.assertEqual(my_model_dict['_class_'], 'BaseModel')
-
+        
     def test_save(self):
-        '''Testing save method that updates the updated_at attr'''
-        initial_update = self.myinstance.updated_at
-        self.myinstance.save()
-        self.assertNotEqual(initial_update, self.myinstance.updated_at)
+       '''Testing save method that updates the updated_at attr'''
+       initialUpdate = self.myinstance.updated_at
+       self.myinstance.save()
+       self.assertNotEqual(initialUpdate, self.myinstance.updated_at)
+       self.assertTrue((self.myinstance.updated_at - self.myinstance.created_at) <= timedelta(seconds=1))
+
+       """ test positional args """
+       with self.assertRaises(TypeError):
+            self.myinstance.save(7)
 
 if __name__ == '__main__':
     unittest.main()
