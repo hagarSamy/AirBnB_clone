3e8fce12e4b7f5423b4f0a15d5a123affcb5fff4
hagarSamy <hs1800@fayoum.edu.eg>
2024-02-13 14:01:11 +0200

    little updates in todict method

--- models/base_model.py
+++ models/base_model.py
@@ -17,20 +17,22 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
+    def __str__(self):
+        '''Beautifying the printed output'''
+
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
         '''Upadtes updated_at to current date time'''
 
         self.updated_at = datetime.now()
 
+    
     def to_dict(self):
         '''Returns a dictionary'''
-
-        self.__dict__["created_at"] = self.created_at.isoformat()
-        self.__dict__["updated_at"] = self.updated_at.isoformat()
-        self.__dict__["__class__"] = self.__class__.__name__
-        return (self.__dict__)
-
-    def __str__(self):
-        '''Beautifying the printed output'''
-
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        mydict = {}
+        for key, value in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                mydict[key] = value.isoformat()
+        mydict['__class__'] = self.__class__.__name__
+        return mydict
