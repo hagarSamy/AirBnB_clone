0724529aaeb77662b36ce540ff34cf61e3f998c5
aya-cs22 <aya786930@gmail.com>
2024-02-13 16:00:13 +0200

    commit to airbnb where base is partially handled

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -20,6 +20,13 @@ class TestBaseModel(unittest.TestCase):
        with self.assertRaises(TypeError):
             self.myinstance.save(7)
 
+    def test_str(self):
+
+        '''Testing the __str__ method of the BaseModel class.'''
+        instance_str = str(self.myinstance)
+        self.assertIn(self.myinstance.__class__.__name__, instance_str)
+        self.assertIn(self.myinstance.id, instance_str)
+        self.assertIn(str(self.myinstance.__dict__), instance_str)
 
 if __name__ == '__main__':
     unittest.main()
