7c3570515e2db0087f93d328c2dc886f743af9f7
hagarSamy <hs1800@fayoum.edu.eg>
2024-03-03 22:41:24 +0200

    teseting save() method

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,6 +2,7 @@
 from models.engine.file_storage import FileStorage
 import unittest
 import models
+import os
 from models.base_model import BaseModel
 '''A module to test storage'''
 
@@ -17,6 +18,9 @@ class TestStorage(unittest.TestCase):
     def test_FileStorage(self):
         self.assertIsInstance(FileStorage._FileStorage__objects, dict)
         self.assertIsInstance(FileStorage._FileStorage__file_path, str)
-
+    def test_save(self):
+        B2 = BaseModel()
+        B2.save()
+        self.assertTrue(os.path.exists("file.json"))
 if __name__ == '__main__':
     unittest.main()
