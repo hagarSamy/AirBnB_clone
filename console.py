396aefc2f0af79695ab99965805898dcdfd4f3a4
HagarSamy <hagarsami63@gmai.com>
2024-03-03 16:48:54 +0200

    console functions

--- console.py
+++ console.py
@@ -56,11 +56,72 @@ class HBNBCommand(cmd.Cmd):
             print("** instance id missing **")
             return
         key = f"{args[0]}.{args[1]}"
-        instance = storage.all()
-        if key in instance:
-            print(instance[key])
+        allinstance = storage.all()
+        if key in allinstance.keys():
+            print(allinstance[key])
         else:
             print("** no instance found **")
+
+    def do_destroy(self, args):
+        '''deletes and instance and save changes to json'''
+
+        if not args:
+            print("** class name missing **")
+            return
+        args = args.split()
+        if args[0] != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = f"{args[0]}.{args[1]}"
+        allinstance = storage.all()
+        if key in allinstance.keys():
+            del allinstance[key]
+            storage.save()
+        else:
+            print("** no instance found **")
+
+    def do_all(self, args):
+        '''Prints all string representation of all
+        instances based or not on the class name'''
+
+        myLs = []
+        args = args.split()
+        if len(args) == 0 or (len(args) > 0 and args[0] == 'BaseModel'):
+            allObj = storage.all()
+            for v in allObj.values():
+                myLs.append(v.__str__())
+        print(myLs)
+
+
+    def do_update(self, args):
+        '''Updates an instance based on the class name
+        and id by adding or updating attribute'''
+
+        if not args:
+            print("** class name missing **")
+            return
+        args = args.split()
+        if args[0] != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = f"{args[0]}.{args[1]}"
+        allinstance = storage.all()
+        if key not in allinstance.keys():
+            print("** no instance found **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        if len(args) < 4:
+            print("** value missing **")
+            return
+        setattr(self, args[2], args[3])
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
