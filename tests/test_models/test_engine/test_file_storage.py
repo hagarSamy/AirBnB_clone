ad69ffccc5788141e5a72b70c1b9a4c2ea7d86c2
hagarSamy <hs1800@fayoum.edu.eg>
2024-03-03 21:40:02 +0200

    teseting new()

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,7 @@
 
 from models.engine.file_storage import FileStorage
 import unittest
+import models
 from models.base_model import BaseModel
 '''A module to test storage'''
 
@@ -10,6 +11,9 @@ class TestStorage(unittest.TestCase):
 
     def setUp(self):
         self.B1 = BaseModel()
+    def test_new(self):
+        B2 = BaseModel()
+        self.assertIn("BaseModel." + B2.id, models.storage.all().keys())
     
 if __name__ == '__main__':
     unittest.main()
